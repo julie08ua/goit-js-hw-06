cf2584b2f0681ecceb104d14a98e0afa15d70903
Yuliia Balabukha <balabukha08@ukr.net>
2023-03-26 17:53:16 +0300

    1-9 tasks

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,14 @@
 const validationInput = document.querySelector('#validation-input');
 
-validationInput.addEventListener('blur', (event) =>
-    event.currentTarget.value.trim().length === Number(validationInput.dataset.length)
-        ? validationInput.classList.add('valid')
-        : validationInput.classList.add('invalid')
+validationInput.addEventListener('blur', (event) => {
+    if (event.currentTarget.value.trim().length === Number(validationInput.dataset.length)) {
+        validationInput.classList.add('valid');
+        validationInput.classList.remove('invalid');
+    } else {
+        validationInput.classList.add('invalid');
+        validationInput.classList.remove('valid');
+    }
+}
 )
-
 
 
